5bff4491555e0e16766623b89fc2b2c172a06ccf
Ключников Евгений Владимирович <EKlyuchnikov@ibs.root.ibscorp.ru>
2018-11-05 10:42:39 +0300

    -

---
+++ src/test/java/ibs/monitoring/livedashboardserver/LiveDashboardServerApplicationTests.java
@@ -0,0 +1,16 @@
+package ibs.monitoring.livedashboardserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiveDashboardServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
