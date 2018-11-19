772a388af0bb93cc0538d90a2627ad1ab0095e1d
Evgeni Kliuchnikov <kluch46@mail.ru>
2018-11-19 09:15:23 +0300

    pace added

--- src/main/java/ibs/monitoring/livedashboardserver/Application.java
+++ src/main/java/ibs/monitoring/livedashboardserver/Application.java
@@ -1,16 +1,17 @@
 package ibs.monitoring.livedashboardserver;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
+//import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.kafka.core.KafkaTemplate;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class Application implements CommandLineRunner {
@@ -21,8 +22,8 @@ public class Application implements CommandLineRunner {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@Autowired
-	private KafkaTemplate<String, String> template;
+//	@Autowired
+//	private KafkaTemplate<String, String> templates;
 
 	private final CountDownLatch latch = new CountDownLatch(3);
 
@@ -35,9 +36,9 @@ public class Application implements CommandLineRunner {
 //		logger.info("All received");
 	}
 
-	@KafkaListener(topics = "myTopic")
-	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
-		logger.info(cr.toString());
-		//latch.countDown();
-	}
+//	@KafkaListener(topics = "myTopic")
+//	public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+//		logger.info(cr.toString());
+//		latch.countDown();
+//	}
 }
